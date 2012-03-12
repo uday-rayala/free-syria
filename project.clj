dfd03d26b71ea103f94a27a2d60ebcbdde421a1d
Andrew Whitehouse <andrew-github@whitehouse.org.uk>
2012-03-12 22:34:09 +0000

    Move user name and API key to properties file

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject free-syria "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.0"]]
+                           [noir "1.2.0"]
+                           [org.clojure/tools.logging "0.2.3"]]
             :main free-syria.server
             :ring {:handler project.server/handler}
             )
